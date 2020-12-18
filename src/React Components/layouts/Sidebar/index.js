8ad190539f4ed854546a42538ad52e5cc278665b
yazhni <yazhni.d@optisolbusiness.com>
2020-12-18 11:49:19 +0530

    updating layouts in react components

--- src/React Components/layouts/Sidebar/index.js
+++ src/React Components/layouts/Sidebar/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
